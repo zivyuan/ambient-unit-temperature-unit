cfd24e184d235e0e6717c0dd25beca9f14445039
Gozi <ziv.yuan@gmail.com>
2020-06-03 03:09:09 +0800

    remove sht add bme280

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,31 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
-// #include <ESP8266WiFiMulti.h>
-#include <ESP8266HTTPClient.h>
+#include "Wire.h"
 #include "ADS1115.h"
-#include "SHTSensor.h"
+#include "BME280_t.h"
+
 
 // #define DEBUG_OFF
 #include "SerialHelper.h"
 
-// ESP8266 work mode configuretion
-// ADC_MODE(ADC_TOUT_3V3);
-
-#define API_BASE     String("http://gemdesign.cn/milk/dailylog.php")
-
-#define LED                  2
+#define LED                  13
 
 #define ADS1115_ALERT        15
 
-#define PM25_LED             14
+#define PM25_LED             3
+
 // PM2.5 sample time mesured as micro second
 #define PM25_SAMPLE_TIME     280
 #define PM25_DELTA_TIME      40
 
+#define ASCII_ESC 27
 
-// == WIFI setup block
-#define AP_NAME       "Gozi2016"
-#define PASSWORD      "pass4share"
+#define MYALTITUDE  150.50
 
-// #define NO_REPORT      1
+char bufout[10];
 
-#ifdef NO_REPORT
+BME280<> BMESensor;
 
-// Report interval 10 minute
-#define REPORT_INTERVAL        5000
 
-#else
-
-// Report interval 10 minute
-#define REPORT_INTERVAL        600000
-
-#endif
 
 /**
  * Convert IP from int
@@ -57,25 +43,17 @@ String ipFromInt(unsigned int ip) {
 String ID;
 // Data report handle
 void report(String event, String data = "") {
-#ifndef NO_REPORT
 
-  HTTPClient http;
-
-  event.replace(" ", "%20");
-  data.replace(" ", "%20");
-
-  String request = API_BASE + "?id=" + ID + "&e=" + event + "&d=" + data;
-
-  http.begin(request);
-  http.GET();
-  http.end();
-
-#endif
 }
 
 
+void initAdc();
+void pollAlertReadyPin();
+void readPM25();
+void blink();
+
+
 ADS1115 adc0(ADS1115_DEFAULT_ADDRESS);
-SHTSensor sht;
 
 void setup() {
 
@@ -89,35 +67,137 @@ void setup() {
 
   delay(100);
 
-  // setupWIFI();
-  int ledOn = 0;
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin("Gozi2016", "pass4share");
-  // WiFi.begin("ChinaNGB-wLxf5w", "KivikGiK");
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-    ledOn = ledOn == 0 ? 1 : 0;
-    digitalWrite(LED, ledOn);
-    sprint(".");
-  }
-
-  ID = WiFi.macAddress();
-  sprintln(" ");
-  sprintln("WIFI connect success");
-  sprintln(ipFromInt(WiFi.localIP()));
-  sprintln(ID);
-
-  // Wait for wifi prepare
-  delay(100);
-
-  //I2Cdev::begin();  // join I2C bus
+  // join I2C bus
   Wire.begin();
+
+  if (!Wire.available()) {
+    sprint("Wire not available.\n");
+  }
 
   // Wait I2C prepare
   delay(100);
+
+  BMESensor.begin();
+
+  initAdc();
+
+
+  // To get output from this method, you'll need to turn on the
+  // #ifdef ADS1115_SERIAL_DEBUG
+  // adc0.showConfigRegister();
+  // sprint("HighThreshold=");
+  // sprintln(adc0.getHighThreshold(),BIN);
+  // sprint("LowThreshold=");
+  // sprintln(adc0.getLowThreshold(),BIN);
+  // #endif
+
+  delay(500);
+
+
+  blink();
+}
+
+int ledState = LOW;
+float dustDensity = 0;
+
+void loop() {
+
+  // readPM25();
+  // delay(5);
+
+  // BME280
+  //
+    BMESensor.refresh();                                                  // read current sensor data
+  // sprintf(bufout,"%c[1;0H",ASCII_ESC);
+  // Serial.println(bufout);
+  Serial.println("BME 280:");
+
+  Serial.print("  Temperature: ");
+  Serial.print(BMESensor.temperature);                                  // display temperature in Celsius
+  Serial.println("C");
+
+  Serial.print("  Humidity:    ");
+  Serial.print(BMESensor.humidity);                                     // display humidity in %
+  Serial.println("%");
+
+  Serial.print("  Pressure:    ");
+  Serial.print(BMESensor.pressure  / 100.0F);                           // display pressure in hPa
+  Serial.println("hPa");
+
+  float relativepressure = BMESensor.seaLevelForAltitude(MYALTITUDE);
+  Serial.print("  RelPress:    ");
+  Serial.print(relativepressure  / 100.0F);                             // display relative pressure in hPa for given altitude
+  Serial.println("hPa");
+
+  Serial.print("  Altitude:    ");
+  Serial.print(BMESensor.pressureToAltitude(relativepressure));         // display altitude in m for given pressure
+  Serial.println("m");
+  //
+  // End of BME280
+
+
+  sprintln();
+
+  blink();
+  blink();
+
+
+  delay(2000);
+
+}
+
+
+
+void blink() {
+  digitalWrite(LED, HIGH);
+  delay(300);
+  digitalWrite(LED, LOW);
+  delay(200);
+}
+
+/** Poll the assigned pin for conversion status
+ */
+void pollAlertReadyPin() {
+  for (uint32_t i = 0; i<100000; i++)
+    if (!digitalRead(ADS1115_ALERT)) return;
+   sprintln("Failed to wait for AlertReadyPin, it's stuck high!");
+}
+
+
+int readFromAdc() {
+  digitalWrite(PM25_LED, HIGH);
+  delayMicroseconds(PM25_SAMPLE_TIME);
+  adc0.setMultiplexer(ADS1115_MUX_P0_NG);
+  adc0.triggerConversion();
+  int data = adc0.getConversionP0GND();
+  digitalWrite(PM25_LED, LOW);
+
+  return data;
+}
+
+void readPM25() {
+  int densityRead = 0;
+  float newDensity = 0;
+  densityRead += readFromAdc();
+  delay(1);
+  densityRead += readFromAdc();
+  delay(1);
+  densityRead += readFromAdc();
+  delay(1);
+  densityRead += readFromAdc();
+  delay(1);
+  densityRead += readFromAdc();
+
+  newDensity = (densityRead / 5) * (3.3 / 32767);
+  dustDensity = dustDensity * 0.17 + newDensity * 0.83;
+  float pm25 = dustDensity * 1000;
+  sprint("PM25: ");
+  sprintln(String(pm25) + " ug/m3");
+}
+
+
+
+void initAdc() {
 
   sprintln("Testing device connections...");
   sprintln(adc0.testConnection() ? "ADS1115 connection successful" : "ADS1115 connection failed");
@@ -137,143 +217,5 @@ void setup() {
 
   // pinMode(ADS1115_ALERT, INPUT_PULLUP);
   // adc0.setConversionReadyPinMode();
-
-  // To get output from this method, you'll need to turn on the
-  //#define ADS1115_SERIAL_DEBUG // in the ADS1115.h file
-  #ifdef ADS1115_SERIAL_DEBUG
-  adc0.showConfigRegister();
-  sprint("HighThreshold="); sprintln(adc0.getHighThreshold(),BIN);
-  sprint("LowThreshold="); sprintln(adc0.getLowThreshold(),BIN);
-  #endif
-
-  if (sht.init()) {
-    sprint("init(): success\n");
-  } else {
-    sprint("init(): failed\n");
-  }
-  sht.setAccuracy(SHTSensor::SHT_ACCURACY_MEDIUM); // only supported by SHT3x
-
-  report("ENV Monitor ONLINE");
-
-  delay(500);
-
-
-  digitalWrite(LED, LOW);
-  delay(2000);
-  digitalWrite(LED, HIGH);
-}
-
-/** Poll the assigned pin for conversion status
- */
-void pollAlertReadyPin() {
-  for (uint32_t i = 0; i<100000; i++)
-    if (!digitalRead(ADS1115_ALERT)) return;
-   sprintln("Failed to wait for AlertReadyPin, it's stuck high!");
-}
-
-int readPM25() {
-  digitalWrite(PM25_LED, HIGH);
-  delayMicroseconds(PM25_SAMPLE_TIME);
-  adc0.setMultiplexer(ADS1115_MUX_P2_NG);
-  adc0.triggerConversion();
-  int data = adc0.getConversionP2GND();
-  digitalWrite(PM25_LED, LOW);
-
-  return data;
-}
-
-int ledState = LOW;
-float dustDensity = 0;
-
-void loop() {
-
-  // ledState = ! ledState;
-  // digitalWrite(LED, ledState);
-  digitalWrite(LED, LOW);
-
-  // Read soil humidity
-  //
-  adc0.setMultiplexer(ADS1115_MUX_P0_NG);
-  adc0.triggerConversion();
-  float soilHumidity = (float)adc0.getConversionP0GND();
-  float shum = (soilHumidity / 32767.0) * 100.0;
-  shum = shum > 100 ? 100 : 0;
-  sprint("Soil Humidity: ");
-  sprintln(String(shum) + "%");
-  report("Soil Humidity", String(shum));
-  //
-  // === END of soil humidity
-
-
-  delay(5);
-
-
-  // Read water level
-  //
-  adc0.setMultiplexer(ADS1115_MUX_P1_NG);
-  adc0.triggerConversion();
-  float rainv = (float)adc0.getConversionP1GND();
-  float rain = (rainv / 32767.0) * 100.0;
-  rain = rain > 100 ? 100 : 0;
-  sprint("Raining: ");
-  sprint(rain);
-  sprintln("%");
-  report("Raining Level", String(rain));
-  //
-  // === END of water level
-
-
-  // Read PM2.5 sensor
-  //
-  int densityRead = 0;
-  float newDensity = 0;
-  densityRead += readPM25();
-  delay(1);
-  densityRead += readPM25();
-  delay(1);
-  densityRead += readPM25();
-  delay(1);
-  densityRead += readPM25();
-  delay(1);
-  densityRead += readPM25();
-
-  newDensity = (densityRead / 5) * (3.3 / 32767);
-  dustDensity = dustDensity * 0.17 + newDensity * 0.83;
-  float pm25 = dustDensity * 1000;
-  sprint("PM25: ");
-  sprintln(String(pm25) + " ug/m3");
-  report("PM25", String(pm25) + " ug/m3");
-  //
-  // === END of PM2.5 sensor
-
-  delay(5);
-
-
-  // SHT3x sensor
-  //
-  if (sht.readSample()) {
-    float humidity = sht.getHumidity();
-    sprint("SHT:\n");
-    sprint("  RH: ");
-    sprint(humidity);
-    sprint("\n");
-    float temperature = sht.getTemperature();
-    sprint("  T:  ");
-    sprint(temperature);
-    sprint("\n");
-
-    report("Humidity", String(humidity));
-    report("Temperature", String(temperature));
-  } else {
-    sprint("Error in readSample()\n");
-  }
-  //
-  // === END of SHT3x sensor
-
-  sprintln();
-
-  digitalWrite(LED, HIGH);
-
-  delay(REPORT_INTERVAL);
 
 }
